1b1e4b75af97177a2491696011b9788ad50903d5
Firtina Ozbalikci <firtina14@gmail.com>
2015-10-13 18:17:08 +0100

    Add edgesGeometry descriptor

---
+++ src/React3/lib/descriptors/Geometry/EdgesGeometryDescriptor.js
@@ -0,0 +1,36 @@
+import THREE from 'three';
+import BufferGeometryDescriptorBase from './BufferGeometryDescriptorBase';
+
+import PropTypes from 'react/lib/ReactPropTypes';
+
+class EdgesGeometryDescriptor extends BufferGeometryDescriptorBase {
+  constructor(react3RendererInstance) {
+    super(react3RendererInstance);
+
+    this.hasProp('thresholdAngle', {
+      type: PropTypes.number,
+      update: this.updateCacheAndReplace.bind(this, 'thresholdAngle'),
+      default: undefined,
+    });
+
+    this.hasProp('geometry', {
+      type: PropTypes.oneOf([
+        THREE.Geometry,
+        THREE.BufferGeometry,
+      ]).isRequired,
+      update: this.updateCacheAndReplace.bind(this, 'geometry'),
+      default: undefined,
+    });
+  }
+
+  construct(props) {
+    const {
+      geometry,
+      thresholdAngle,
+      } = props;
+
+    return new THREE.EdgesGeometry(geometry, thresholdAngle);
+  }
+}
+
+export default EdgesGeometryDescriptor;
