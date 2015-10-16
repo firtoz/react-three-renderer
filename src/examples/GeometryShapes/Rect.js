86a1182bd6cd48dce5c2bb156846e8b4a0c070d3
Firtina Ozbalikci <firtina14@gmail.com>
2015-10-17 00:05:48 +0100

    Convert Rect to stateless function

--- src/examples/GeometryShapes/Rect.js
+++ src/examples/GeometryShapes/Rect.js
@@ -1,47 +1,50 @@
 import React from 'react';
 import PropTypes from 'react/lib/ReactPropTypes';
+import PureComponentMixin from 'react/lib/ReactComponentWithPureRenderMixin';
 
-class Rect extends React.Component {
-  static propTypes = {
-    width: PropTypes.number.isRequired,
-    length: PropTypes.number.isRequired,
-    resourceId: PropTypes.string.isRequired,
-  };
+function Rect(props) {
+  const {
+    width,
+    length,
+    resourceId,
+    } = props;
 
-  render() {
-    const {
-      width,
-      length,
-      resourceId,
-      } = this.props;
+  // console.log('wat');
+  //
+  // this.shouldComponentUpdate = function asdf(){
+  //   console.log('should I update huh?');
+  //
+  //   return PureComponentMixin.shouldComponentUpdate.apply(this, arguments);
+  // };
 
-    return (<shape resourceId={resourceId}>
-      <moveTo
-        x={0}
-        y={0}
-      />
-      <lineTo
-        x={0}
-        y={width}
-      />
-      <lineTo
-        x={length}
-        y={width}
-      />
-      <lineTo
-        x={length}
-        y={width}
-      />
-      <lineTo
-        x={length}
-        y={0}
-      />
-      <lineTo
-        x={0}
-        y={0}
-      />
-    </shape>);
-  }
+  return (<shape resourceId={resourceId}>
+    <moveTo
+      x={0}
+      y={0}
+    />
+    <lineTo
+      x={0}
+      y={width}
+    />
+    <lineTo
+      x={length}
+      y={width}
+    />
+    <lineTo
+      x={length}
+      y={0}
+    />
+    <lineTo
+      x={0}
+      y={0}
+    />
+  </shape>);
 }
+
+Rect.propTypes = {
+  width: PropTypes.number.isRequired,
+  length: PropTypes.number.isRequired,
+  resourceId: PropTypes.string.isRequired,
+};
 
 export default Rect;
