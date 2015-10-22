a5ef5507a74a4b9f337303f2028d368d63948188
Firtina Ozbalikci <firtina14@gmail.com>
2015-10-22 23:36:45 +0100

    Create uuid for module types

--- src/Module.js
+++ src/Module.js
@@ -1,6 +1,9 @@
+import THREE from 'three';
+
 class React3Module {
   constructor() {
     this.userData = {};
+    this.uuid = THREE.Math.generateUUID();
   }
 
   setup(react3RendererInstance) { // eslint-disable-line no-unused-vars
