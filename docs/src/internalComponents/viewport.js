25aef055a7d074254a7f143ac023aa540a4a47d5
Firtina Ozbalikci <firtina14@gmail.com>
2015-10-28 08:57:42 +0000

    [docs] create viewport's intro string and fix link to mainCamera

--- docs/src/internalComponents/viewport.js
+++ docs/src/internalComponents/viewport.js
@@ -1,13 +1,15 @@
 import DocInfo from '../DocInfo';
 
 class Viewport extends DocInfo {
+  getIntro() {
+    return `See [THREE.WebGLRenderer.setViewport](http://threejs.org/docs/#Reference/Renderers/WebGLRenderer.setViewport)`;
+  }
+
   getDescription() {
     return `
-See [THREE.WebGLRenderer.setViewport](http://threejs.org/docs/#Reference/Renderers/WebGLRenderer.setViewport)
-
 It can only be placed into the [root component](React3).
 
-If the [mainCamera](React3#mainCamera) property of the root component is
+If the [mainCamera](react3#maincamera) property of the root component is
 not set, then the scene will be rendered into the viewports.`;
   }
 
