119f26e8e8ecdd540ad70f45bd85ff27b73c31fe
Firtina Ozbalikci <firtina14@gmail.com>
2015-10-30 18:22:46 +0000

    [docs] camera.name

--- docs/src/internalComponents/camera.js
+++ docs/src/internalComponents/camera.js
@@ -12,6 +12,9 @@ class camera extends object3D {
   getAttributesText() {
     return {
       ...super.getAttributesText(),
+      name: 'The [name]() property of the camera.\n\n' +
+      'This gets used by [&lt;react3/&gt;](react3), [&lt;viewport/&gt;](viewport), or' +
+      ' [&lt;cameraHelper&gt;](cameraHelper).',
       near: 'The [near]() property of the camera.',
       far: 'The [far]() property of the camera.',
     };
