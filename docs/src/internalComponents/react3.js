88433ad56eaf4c2800c3ec33c4de503cf21a6dd2
Firtina Ozbalikci <firtina14@gmail.com>
2015-11-01 19:05:10 +0000

    [docs] Fix links for react3

--- docs/src/internalComponents/react3.js
+++ docs/src/internalComponents/react3.js
@@ -12,11 +12,11 @@ class react3 extends DocInfo {
   getAttributesText() {
     return {
       gammaInput: 'Controls renderer gamma input.\n\n' +
-      'See [THREE.WebGLRenderer#gammaInput]().',
+      'See [THREE.WebGLRenderer#gammaInput](http://threejs.org/docs/#Reference/Renderers/WebGLRenderer.gammaInput).',
       gammaOutput: 'Controls renderer gamma output.\n\n' +
-      'See [THREE.WebGLRenderer#gammaOutput]().',
+      'See [THREE.WebGLRenderer#gammaOutput](http://threejs.org/docs/#Reference/Renderers/WebGLRenderer.gammaOutput).',
       sortObjects: 'Controls renderer object sorting.\n\n' +
-      'See [THREE.WebGLRenderer#sortObjects]().',
+      'See [THREE.WebGLRenderer#sortObjects](http://threejs.org/docs/#Reference/Renderers/WebGLRenderer.sortObjects).',
       context: 'The rendering context.\n\n' +
       `Currently only '3d' is supported.`,
       mainCamera: 'The name of the camera to render into the canvas.\n\n' +
@@ -26,14 +26,19 @@ class react3 extends DocInfo {
       canvas: 'The canvas to render into.\n\n' +
       'Internal usage only.',
       onAnimate: 'This callback gets called before every frame.',
-      clearColor: 'The clear color of the renderer.',
+      clearColor: 'The clear color of the renderer.\n\n' +
+      'See [THREE.WebGLRenderer#clearColor](http://threejs.org/docs/#Reference/Renderers/WebGLRenderer.clearColor).',
       shadowMapEnabled: 'Toggles shadowMap usage.\n\n' +
+      'See [THREE.WebGLRenderer#shadowMapEnabled](http://threejs.org/docs/#Reference/Renderers/WebGLRenderer.shadowMapEnabled).\n\n' +
       '> **WARNING**: Updating this value will force all materials to refresh.',
       shadowMapType: 'Controls the shadowMap type.\n\n' +
+      'See [THREE.WebGLRenderer#shadowMapType](http://threejs.org/docs/#Reference/Renderers/WebGLRenderer.shadowMapType).\n\n' +
       '> **WARNING**: Updating this value will force all materials to refresh.',
       shadowMapCullFace: 'Controls shadowMap face culling.\n\n' +
+      'See [THREE.WebGLRenderer#shadowMapCullFace](http://threejs.org/docs/#Reference/Renderers/WebGLRenderer.shadowMapCullFace).\n\n' +
       '> **WARNING**: Updating this value will force all materials to refresh.',
       shadowMapDebug: 'Toggles shadowMap debugging.\n\n' +
+      'See [THREE.WebGLRenderer#shadowMapDebug](http://threejs.org/docs/#Reference/Renderers/WebGLRenderer.shadowMapDebug).\n\n' +
       '> **WARNING**: Updating this value will force all materials to refresh.',
       onRecreateCanvas: 'This callback gets called every time the canvas is recreated.\n\n' +
       'This is currently internal usage only. This helps toggling of [antialias](#antialias).',
@@ -42,6 +47,7 @@ class react3 extends DocInfo {
       width: 'The height of the canvas and the default viewport.',
       height: 'The width of the canvas and the default viewport.',
       antialias: 'Toggles anti-aliasing of the renderer.\n\n' +
+      'See [THREE.WebGLRenderer#antialias](http://threejs.org/docs/#Reference/Renderers/WebGLRenderer.antialias).\n\n' +
       '> **WARNING**: This recreates the whole canvas.',
     };
   }
