6c1213b9b7125b0e8abd999326b03e0c19d1928b
Firtina Ozbalikci <firtina14@gmail.com>
2016-03-10 21:05:15 +0000

    [core] Enable replacement of composite components
    With internal components and vice versa.

--- tests/src/core/React3/React3Mounts.js
+++ tests/src/core/React3/React3Mounts.js
@@ -39,4 +39,202 @@ module.exports = type => {
 
     expect(canvas.userData).to.exist();
   });
+
+  it('can replace internal components with composites and vice versa', () => {
+    ReactDOM.render(<React3
+      width={800}
+      height={600}
+    >
+      <scene/>
+    </React3>, testDiv);
+
+    mockConsole.expect('THREE.WebGLRenderer	74');
+
+    let mounted = false;
+
+    /* eslint-disable react/no-multi-comp */
+
+    class MyScene extends React.Component {
+      componentDidMount() {
+        mounted = true;
+      }
+
+      componentWillUnmount() {
+        mounted = false;
+      }
+
+      render() {
+        return (<scene/>);
+      }
+    }
+
+    class MyResources extends React.Component {
+      render() {
+        return (<resources/>);
+      }
+    }
+
+    class Wrapper extends React.Component {
+      static propTypes = {
+        internal: React.PropTypes.bool,
+        res: React.PropTypes.bool,
+      };
+
+      render() {
+        if (this.props.internal) {
+          return (<scene/>);
+        }
+
+        if (this.props.res) {
+          return (<MyResources/>);
+        }
+
+        return (<MyScene/>);
+      }
+    }
+
+    /* eslint-enable */
+
+    ReactDOM.render(<React3
+      width={800}
+      height={600}
+    >
+      <Wrapper
+        internal
+      />
+    </React3>, testDiv);
+
+    expect(mounted).to.equal(false);
+
+    ReactDOM.render(<React3
+      width={800}
+      height={600}
+    >
+      <Wrapper internal={false}/>
+    </React3>, testDiv);
+
+    expect(mounted).to.equal(true);
+
+    ReactDOM.render(<React3
+      width={800}
+      height={600}
+    >
+      <Wrapper
+        internal
+      />
+    </React3>, testDiv);
+
+    expect(mounted).to.equal(false);
+
+    ReactDOM.render(<React3
+      width={800}
+      height={600}
+    >
+      <Wrapper
+        res
+      />
+    </React3>, testDiv);
+  });
+
+  it('can replace internal components with composites and vice versa ' +
+    'with a previous sibling', () => {
+    ReactDOM.render(<React3
+      width={800}
+      height={600}
+    >
+      <viewport x={0} y={0} width={0} height={0} cameraName="test"/>
+      <scene/>
+    </React3>, testDiv);
+
+    mockConsole.expect('THREE.WebGLRenderer	74');
+
+    let mounted = false;
+
+    /* eslint-disable react/no-multi-comp */
+
+    class MyScene extends React.Component {
+      componentDidMount() {
+        mounted = true;
+      }
+
+      componentWillUnmount() {
+        mounted = false;
+      }
+
+      render() {
+        return (<scene/>);
+      }
+    }
+
+    class MyResources extends React.Component {
+      render() {
+        return (<resources/>);
+      }
+    }
+
+    class Wrapper extends React.Component {
+      static propTypes = {
+        internal: React.PropTypes.bool,
+        res: React.PropTypes.bool,
+      };
+
+      render() {
+        if (this.props.internal) {
+          return (<scene/>);
+        }
+
+        if (this.props.res) {
+          return (<MyResources/>);
+        }
+
+        return (<MyScene/>);
+      }
+    }
+
+    /* eslint-enable */
+
+    ReactDOM.render(<React3
+      width={800}
+      height={600}
+    >
+      <viewport x={0} y={0} width={0} height={0} cameraName="test"/>
+      <Wrapper
+        internal
+      />
+    </React3>, testDiv);
+
+    expect(mounted).to.equal(false);
+
+    ReactDOM.render(<React3
+      width={800}
+      height={600}
+    >
+      <viewport x={0} y={0} width={0} height={0} cameraName="test"/>
+      <Wrapper internal={false}/>
+    </React3>, testDiv);
+
+    expect(mounted).to.equal(true);
+
+    ReactDOM.render(<React3
+      width={800}
+      height={600}
+    >
+      <viewport x={0} y={0} width={0} height={0} cameraName="test"/>
+      <Wrapper
+        internal
+      />
+    </React3>, testDiv);
+
+    expect(mounted).to.equal(false);
+
+    ReactDOM.render(<React3
+      width={800}
+      height={600}
+    >
+      <viewport x={0} y={0} width={0} height={0} cameraName="test"/>
+      <Wrapper
+        res
+      />
+    </React3>, testDiv);
+  });
 };
