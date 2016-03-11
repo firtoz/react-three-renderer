ecbd1fc2fd852ab15fd752fea8ef07a65f76bfca
Firtina Ozbalikci <firtina14@gmail.com>
2016-03-11 18:22:47 +0000

    [components] Remove unused property from rawShaderMaterial
    Fixes #34

--- src/lib/descriptors/Material/RawShaderMaterialDescriptor.js
+++ src/lib/descriptors/Material/RawShaderMaterialDescriptor.js
@@ -2,6 +2,16 @@ import THREE from 'three';
 import ShaderMaterialDescriptor from './ShaderMaterialDescriptor';
 
 class RawShaderMaterialDescriptor extends ShaderMaterialDescriptor {
+  constructor(react3RendererInstance) {
+    super(react3RendererInstance);
+
+    [
+      'alphaTest',
+    ].forEach(propName => {
+      this.removeProp(propName);
+    });
+  }
+
   construct(props) {
     const materialDescription = this.getMaterialDescription(props);
 
