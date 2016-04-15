bd00f07bddf5c5f29d15a6595c49e2ca1806d8b7
Pep Rodeja <pepjo26@gmail.com>
2016-04-15 13:16:57 +0200

    Add to element descriptor container

--- src/lib/ElementDescriptorContainer.js
+++ src/lib/ElementDescriptorContainer.js
@@ -80,6 +80,7 @@ import SpriteMaterialDescriptor from './descriptors/Material/SpriteMaterialDescr
 import CameraHelperDescriptor from './descriptors/Object/Helper/CameraHelperDescriptor';
 import AxisHelperDescriptor from './descriptors/Object/Helper/AxisHelperDescriptor';
 import ArrowHelperDescriptor from './descriptors/Object/Helper/ArrowHelperDescriptor';
+import GridHelperDescriptor from './descriptors/Object/Helper/GridHelperDescriptor';
 
 class ElementDescriptorContainer {
   constructor(react3RendererInstance) {
@@ -172,6 +173,7 @@ class ElementDescriptorContainer {
       cameraHelper: new CameraHelperDescriptor(react3RendererInstance),
       axisHelper: new AxisHelperDescriptor(react3RendererInstance),
       arrowHelper: new ArrowHelperDescriptor(react3RendererInstance),
+      gridHelper: new GridHelperDescriptor(react3RendererInstance),
     };
   }
 }
