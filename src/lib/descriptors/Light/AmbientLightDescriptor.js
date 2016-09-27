64ac3431b3027f7b4f5d14e2acdba454ce14a5b8
Arthur Schwaiger <arthur.schwaiger@gmail.com>
2016-09-27 13:35:26 +0200

    #109 AmbientLight: add 'intensity' prop

--- src/lib/descriptors/Light/AmbientLightDescriptor.js
+++ src/lib/descriptors/Light/AmbientLightDescriptor.js
@@ -1,10 +1,18 @@
 import THREE from 'three';
 
+import PropTypes from 'react/lib/ReactPropTypes';
+
 import LightDescriptorBase from './LightDescriptorBase';
 
 class AmbientLightDescriptor extends LightDescriptorBase {
   constructor(react3Instance) {
     super(react3Instance);
+
+    this.hasProp('intensity', {
+      type: PropTypes.number,
+      simple: true,
+      default: 1,
+    });
 
     this.hasColor();
   }
@@ -12,9 +20,10 @@ class AmbientLightDescriptor extends LightDescriptorBase {
   construct(props) {
     const {
       color,
-      } = props;
+      intensity,
+    } = props;
 
-    return new THREE.AmbientLight(color);
+    return new THREE.AmbientLight(color, intensity);
   }
 }
 
