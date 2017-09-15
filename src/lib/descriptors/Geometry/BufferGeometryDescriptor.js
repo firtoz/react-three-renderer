54ea395887e2f713ad00a42719278084a29d41c9
John Villar <johnvillar@contimita.com>
2017-09-15 16:09:36 -0400

    Fixed more eslint errors

--- src/lib/descriptors/Geometry/BufferGeometryDescriptor.js
+++ src/lib/descriptors/Geometry/BufferGeometryDescriptor.js
@@ -55,12 +55,12 @@ class BufferGeometryDescriptor extends GeometryDescriptorBase {
 
     this.hasProp('groups', {
       type: PropTypes.oneOfType([
-        propTypeInstanceOf(Array)
+        propTypeInstanceOf(Array),
       ]),
       update(threeObject, groupArray) {
         groupArray.forEach((group) => {
           threeObject.addGroup(group.start, group.count, group.materialIndex);
-        })
+        });
       },
       updateInitial: true,
       default: undefined,
